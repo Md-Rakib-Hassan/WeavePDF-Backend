9534bd03b379728c8308d36fcf47a54b264e8e6d
Maliha <maliha16tabassum@gmail.com>
2024-02-05 00:12:11 +0600

    get api for uploaded files

--- src/routes/v1/services/index.js
+++ src/routes/v1/services/index.js
@@ -6,7 +6,10 @@ const htmlToPdf = require("../../../api/v1/services/htmlToPdf");
 
 const router = express.Router()
 
-
+router.get("/upload-file", async(req, res)=>{
+    const result = await Services.find();
+    res.send(result)
+})
 
 router.post('/convertToPDF', htmlToPdf);
 
