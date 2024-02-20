366b587ed6d78e8aeb00c803b2738c578bfef870
Maliha <maliha16tabassum@gmail.com>
2024-02-21 05:15:45 +0600

    added find by id in delete api

--- src/api/v1/services/deleteService.js
+++ src/api/v1/services/deleteService.js
@@ -2,9 +2,8 @@ const express = require("express");
 const Services = require("../../../models/Services");
 
 const deleteService = async(req,res)=>{
-    const id = req.params;
-    const query = { _id: new ObjectId(id) };
-    const result = await Services.findOneAndDelete(query).exec();
+    const {id} = req.params;
+    const result = await Services.findByIdAndDelete(id).exec();
     res.send(result);
 }
 
