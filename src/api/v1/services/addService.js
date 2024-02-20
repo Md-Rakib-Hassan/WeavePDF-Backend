80108607d313f2c106d9e47416c0608d73149080
Maliha <maliha16tabassum@gmail.com>
2024-02-20 21:44:03 +0600

    file adds as binary data in mongo

--- src/api/v1/services/addService.js
+++ src/api/v1/services/addService.js
@@ -1,18 +1,42 @@
 const express = require("express");
 const Services = require("../../../models/Services");
-
+const fs = require('fs');
+const { log } = require("console");
 
 const app = express()
 
 app.use(express.json())
 
+const fileMimeType = ['application/pdf', 'application/msword']
+
 const addService = async(req,res)=>{
-    const {date, user_email, no_of_files, service_name, status, file} = req.body;
+    const {date, user_email, no_of_files, service_name, status, pdf} = req.body;
+    // console.log(file);
     // console.log(req.body);
-  const service = new Services({
-    date, user_email, no_of_files, service_name, status
+    // const pdfFile = fs.readFileSync(file)
+    const file = new Buffer.from(pdf).toString('base64');
+    const service = new Services({
+    date, user_email, no_of_files, service_name, status, file
   })
-  console.log("service to be added: ",service);
+//   savepdf(service,file)
+
+  try{
+    const newService = await service.save()
+  }catch(err){
+    console.log(err);
+  }
+//   console.log("service to be added: ",service);
+}
+
+//save pdf as binary
+
+const savepdf = (service, pdfencoded) =>{
+    if(!pdfencoded) return;
+    const pdf = JSON.parse(JSON.stringify(pdfencoded));
+    // console.log("parsed: ", pdfencoded);
+    if(pdf && fileMimeType.includes(pdf.type)){
+        service.pdf = new Buffer.from(pdf.data, 'base64')
+    }
 }
 
 module.exports = addService;
