087dbad871f9815912bda7356b3212e601f02472
tabib-e-alahi <etabib.alahi@gmail.com>
2024-02-27 13:13:35 +0600

    registration response added

--- src/app.js
+++ src/app.js
@@ -5,31 +5,30 @@ const connectDB = require("./db/connectDB");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
-const authRoutes = require('./routes/v1/authentication/index');
-const serviceRoutes = require('./routes/v1/services/index')
-const subscriptionRoutes = require('./routes/v1/subscription/index')
+const authRoutes = require("./routes/v1/authentication/index");
+const serviceRoutes = require("./routes/v1/services/index");
+const subscriptionRoutes = require("./routes/v1/subscription/index");
 const Reviews = require("./models/Reviews");
 // const adminContact = require("./models/adminContact");
 // const adminContact = require('./routes/v1/adminContact/index');
 
 applyMiddleware(app);
 
-
-
-app.use(authRoutes)
-app.use(serviceRoutes)
-app.use(subscriptionRoutes)
+app.use(authRoutes);
+app.use(serviceRoutes);
+app.use(subscriptionRoutes);
 // app.use(adminContact)
 
-app.use("/files",express.static("files")) //this needs to be done for permission to access from anywhere
+app.use("/files", express.static("files")); //this needs to be done for permission to access from anywhere
 app.get("/", (req, res) => {
   res.send("doctor is running....");
 });
 
-
 // handling all (get,post,update,delete.....) unhandled routes
 app.all("*", (req, res, next) => {
-  const error = new Error(`Can't find ${req.originalUrl} on the server please respond`);
+  const error = new Error(
+    `Can't find ${req.originalUrl} on the server please respond`
+  );
   error.status = 404;
   next(error);
 });
@@ -37,15 +36,11 @@ app.all("*", (req, res, next) => {
 // error handling middleware
 // app.use(globalErrorHandler);
 
+const main = async () => {
+  await connectDB();
+  app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
+};
 
-
-const main=async ()=>{
-    await connectDB()
-    app.listen(port, () => {
-        console.log(`Server is running on port ${port}`);
-    });
-   
-}
-
-
-main()
+main();
