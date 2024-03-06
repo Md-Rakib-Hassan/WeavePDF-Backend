f494ac3f070bd5d2a2e75b46153c234ace2a1ff1
Md-Rakib-Hassan <md.rakibhassan4591@gmail.com>
2024-03-06 16:36:16 +0600

    Added voice alart for non-convertable pdf file

--- src/api/v1/services/pdfToAudioBook.js
+++ src/api/v1/services/pdfToAudioBook.js
@@ -6,10 +6,25 @@ const extractText= (req, res) => {
         res.status(400);
         res.end();
     }
+    
   
-    pdfParse(req.files.pdfFile).then(result => {
-        res.send(result.text);
-    });
+        pdfParse(req.files.pdfFile).then(result => {
+            console.log(result);
+            if(!/[A-Za-z]/.test(result.text)){
+                console.log(result.text);
+                res.send('We cant convert this pdf file, please try again with a different pdf file.');
+            }
+            else{
+                console.log(result.text);
+                res.send(result.text);
+                
+            }
+            
+            
+        });
+    
+   
+    
   }
 
   module.exports = extractText;
